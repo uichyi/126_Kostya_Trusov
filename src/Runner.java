ad6858a1772413ee6181091146a32f1b480d8771
uichyi <trusov-kostya@mail.ru>
2021-10-17 15:11:48 +0300

    rest

--- src/Runner.java
+++ src/Runner.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите свою фамилию и имя");
+        System.out.println("Трусов Костя");
         String name = scanner.nextLine();
         Task_1 task = new Task_1();
         Task_1_Tests.test(task, name);
