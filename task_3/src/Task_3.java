fd8d1dc0afe5f1088c99b54c9c22af29f675ff9a
uichyi <trusov-kostya@mail.ru>
2021-10-17 16:00:47 +0300

    task 3

--- task_3/src/Task_3.java
+++ task_3/src/Task_3.java
@@ -3,7 +3,22 @@ public class Task_3 implements Task_3_base {
     public int subtask_1_for(int n1, int n2, int a, int b) {
         // подсчитать, сколько чисел, кратных a, но не кратных b,
         // находится между числами n1 и n2 включительно
-        return 0;
+        int c = 0;
+        if (n1 > n2) {
+            for (int i = n2; i <= n1; ++i) {
+                if (i % a == 0 && i % b != 0) {
+                    ++c;
+                }
+            }
+        }
+        else {
+            for (int i = n1; i <= n2; ++i) {
+                if (i % a == 0 && i % b != 0) {
+                    ++c;
+                }
+            }
+        }
+        return c;
     }
 
     @Override
@@ -17,7 +32,12 @@ public class Task_3 implements Task_3_base {
         // ...
         // Найти, какое число будет находиться в этой последовательности
         // на позиции num
-        return 0;
+        int a = 0;
+        for (int i = 1, b = 1; b <= num; ++i) {
+            b = b + i;
+            a = i;
+        }
+        return a;
     }
 
     @Override
@@ -26,7 +46,15 @@ public class Task_3 implements Task_3_base {
         // a(0) = num
         // a(n) = a(n - 1) * d + 1
         // Найти сумму первых cnt элементов последовательности
-        return 0;
+        int a = num;
+        int a0 = num;
+        int an;
+        for (int i = 1; i < cnt; ++i) {
+            an = a0 * d + 1;
+            a0 = an;
+            a = a + a0;
+        }
+        return a;
     }
 
     @Override
@@ -35,6 +63,12 @@ public class Task_3 implements Task_3_base {
         // S(n) = 1 + 1 * 2 + 1 * 2 * 3 + ... + n!
         // для заданного n
         // (n! - это n-факториал. Кто не знает - гуглите)
-        return 0;
+        int sn = 1;
+        int a = 0;
+        for (int i = 1; i <= n; ++i) {
+            sn = sn * i;
+            a = a + sn;
+        }
+        return a;
     }
 }
