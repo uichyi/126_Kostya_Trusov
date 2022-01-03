99b382d193625ef40a2bb5333fb4425dc6e24263
uichyi <trusov-kostya@mail.ru>
2022-01-03 16:53:12 +0300

    тест завершен + выполнена третья фича

--- task_10/src/deal_list_core/DealList.java
+++ task_10/src/deal_list_core/DealList.java
@@ -2,16 +2,24 @@ package deal_list_core;
 
 public class DealList {
     private int contractsCount;
+    private int paymentDocumentsCount;
     private DealList(){
         contractsCount = 0;
+        paymentDocumentsCount = 0;
     }
     public void addContract(String number, String date){
         ++contractsCount;
+    }
+    public void addPaymentDocument(int sum, int num, String documentType, String number, String date){
+        ++paymentDocumentsCount;
     }
     public int getContractsCount(){
         return contractsCount;
     }
     public static DealList create(){
         return new DealList();
+    }
+    public int getPaymentDocumentsCount(){
+        return paymentDocumentsCount;
     }
 }
