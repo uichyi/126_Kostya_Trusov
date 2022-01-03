04257c65872b438d1e260e53bcd4460dbe90a776
uichyi <trusov-kostya@mail.ru>
2022-01-03 15:50:49 +0300

    добавлен код-проверка тесту (результат зеленый)

---
+++ task_10/src/deal_list_core/DealList.java
@@ -0,0 +1,10 @@
+package deal_list_core;
+
+public class DealList {
+    public int getContractsCount(){
+        return 0;
+    }
+    public static DealList create(){
+        return new DealList();
+    }
+}
