84f1cd0cc5f61c42dc5787e70b71e6a0ea0c500d
uichyi <trusov-kostya@mail.ru>
2022-01-03 15:36:35 +0300

    создан тест create_CreateFirstContractWithGivenNumberAndDate_ContractCountEqualsZero

--- task_10/src/deal_list_tests/DealListTests.java
+++ task_10/src/deal_list_tests/DealListTests.java
@@ -3,5 +3,9 @@ package deal_list_tests;
 import org.junit.*;
 
 public class DealListTests extends Assert{
-    
+    @Test
+    public void create_CreateFirstContractWithGivenNumberAndDate_ContractCountEqualsZero(){
+        DealList dealList = DealList.create();
+        assertEquals(0, dealList.getContractsCount());
+    }
 }
