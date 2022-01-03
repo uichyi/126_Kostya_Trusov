b9200cb01f0f318b4906156199827159de69422e
uichyi <trusov-kostya@mail.ru>
2022-01-03 16:13:43 +0300

    тест завершен + выполнена первая фича

--- task_10/src/deal_list_core/DealList.java
+++ task_10/src/deal_list_core/DealList.java
@@ -1,8 +1,15 @@
 package deal_list_core;
 
 public class DealList {
+    private int contractsCount;
+    private DealList(){
+        contractsCount = 0;
+    }
+    public void addContract(String number, String date){
+        ++contractsCount;
+    }
     public int getContractsCount(){
-        return 0;
+        return contractsCount;
     }
     public static DealList create(){
         return new DealList();
